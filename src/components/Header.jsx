709938454e3e87734c02561c532b1d039c16716c
谭旭 <hyden@hyden.local>
2018-02-27 15:37:54 +0800

    优化代码结构

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,19 @@
+import React from 'react';
+import { Layout, Menu  } from 'antd';
 
+const { Header } = Layout;
 
+export default () => (
+    <Header className="header">
+        <div className="logo" />
+        <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+        >
+            <Menu.Item key="1">文档</Menu.Item>
+            <Menu.Item key="2">题库</Menu.Item>
+        </Menu>
+    </Header>
+)
--- src/routes/MainLayoutRoute.jsx
+++ src/routes/MainLayoutRoute.jsx
@@ -1,62 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
-import { Layout, Menu, Icon, Breadcrumb } from 'antd';
+import { Layout, Breadcrumb } from 'antd';
+import Header from '../components/Header';
+import Sider from '../components/Slider';
 
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
-
-
-const renderHeader = () => (
-    <Header className="header">
-        <div className="logo" />
-        <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-        >
-            <Menu.Item key="1">文档</Menu.Item>
-            <Menu.Item key="2">题库</Menu.Item>
-        </Menu>
-    </Header>
-);
-
-const renderSider = () => (
-    <Sider width={200} style={{ background: '#fff' }}>
-        <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}
-        >
-            <SubMenu key="sub1" title={<span><Icon type="user" />基础</span>}>
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" title={<span><Icon type="laptop" />进阶</span>}>
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3" title={<span><Icon type="notification" />延生</span>}>
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
-        </Menu>
-    </Sider>
-);
+const { Content } = Layout;
 
 const MainLayoutRoute = ({ component: Component, ...rest }) => (
     <Route
         render={props => (
             <Layout {...rest}>
-                {renderHeader()}
+                <Header/>
                 <Layout>
                     <Layout style={{ padding: '0 24px 24px' }}>
                         <Breadcrumb style={{ margin: '16px 0' }}>
@@ -66,7 +21,7 @@ const MainLayoutRoute = ({ component: Component, ...rest }) => (
                             <Component {...props} />
                         </Content>
                     </Layout>
-                    {renderSider()}
+                    <Sider/>
                 </Layout>
             </Layout>
         )}
