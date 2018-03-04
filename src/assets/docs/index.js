d5df5f10d5a96e1ad3cf30fb7d0fe889e5646c5b
谭旭 <hyden@hyden.local>
2018-03-04 19:56:17 +0800

    优化：文档代码高亮显示

---
+++ src/assets/docs/index.js
@@ -0,0 +1,5 @@
+import helloWorld from './helloWorld';
+
+export default {
+    helloWorld
+}
--- src/pages/doc/DocPage.js
+++ src/pages/doc/DocPage.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import marked from 'marked';
+import marked, { Renderer } from 'marked';
+import hljs from 'highlight.js'
+import "highlight.js/styles/vs.css";
+
+import docs from '../../assets/docs';
 
 export default class DocPage extends React.Component {
+    state = {
+        doc: ''
+    }
+
+    componentDidMount() {
+
+        // window.hljs = require('highlight.js');
+        // require('highlightjs-line-numbers.js');
+
+        const doc = this.getDoc();
+        const renderer = new Renderer();
+
+        // window.hljs.initHighlightingOnLoad();
+        // window.hljs.initLineNumbersOnLoad();
+
+        hljs.initHighlightingOnLoad();
+        renderer.code = (code, language) => {
+            // Check whether the given language is valid for highlight.js.
+            const validLang = !!(language && window.hljs.getLanguage(language));
+            // Highlight only if the language is valid.
+            const highlighted = validLang ? window.hljs.highlight(language, code).value : code;
+            // Render the highlighted code with `hljs` class.
+            return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+        };
+
+        // Set the renderer to marked.
+        marked.setOptions({ renderer });
+        this.setState({ doc: marked(doc.content)})
+    }
+
     getDoc() {
         const { location } = this.props;
         const title = location.pathname.replace('/doc/', '');
-        return require(`../../assets/docs/${title}`);
+        return docs[title]
     }
 
     render() {
-        const doc = this.getDoc();
-        const content = marked(doc.content);
-
         return (
-            <div style={{ width: '100%' }} dangerouslySetInnerHTML={{ __html: content }} />
+            <div style={{ width: '100%', wordWrap: 'break-word' }}
+                 dangerouslySetInnerHTML={{ __html: this.state.doc }}
+            />
         )
     }
 }
